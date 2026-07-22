1fe937c64356ad0ac2d0104fd2199e824f61f3f3
Johnny-32 <janglowacki32@gmail.com>
2026-07-22 12:16:06 +0200

    Added first leaflet markers

--- scripts.js
+++ scripts.js
@@ -5,6 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+let londonMarker = L.marker([51.5, -0.09]).addTo(map).bindPopup("London");
+let berlinMarker = L.marker([52.52, 13.4]).addTo(map).bindPopup("Berlin");
+
