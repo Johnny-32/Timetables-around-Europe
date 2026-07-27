497d552f71768a61366a72515a78f7373d194bda
Johnny-32 <janglowacki32@gmail.com>
2026-07-27 16:52:43 +0200

    Added Munich

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let map = L.map('map').setView([49.95122, 10.107422], 4);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
+  maxZoom: 12,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
@@ -14,8 +14,9 @@ let barcelonaMarker = L.marker([41.383333, 2.183333]).addTo(map).bindPopup('<a h
 let stockholmMarker = L.marker([59.329444, 18.068611]).addTo(map).bindPopup('<a href="https://sl.linjetidtabeller.se/" target="_blank">Stockholm</a>');
 let londonMarker = L.marker([51.507222, -0.1275]).addTo(map).bindPopup('<a href="https://tfl.gov.uk/travel-information/timetables/" target="_blank">London</a>');
 let amsterdamMarker = L.marker([52.372778, 4.893611]).addTo(map).bindPopup('<a href="https://www.gvb.nl/en/travel-information/stops-and-timetable" target="_blank">Amsterdam</a>');
-let warsawMarker = L.marker([52.23, 21.011111]).addTo(map).bindPopup('<a href="https://www.wtp.waw.pl/rozklady-jazdy/" target="_blank">Amsterdam</a>');
-let parisMarker = L.marker([48.8567, 2.3522]).addTo(map).bindPopup('<a href="https://www.ratp.fr/en/horaires-bus" target="_blank">Berlin</a>');
+let warsawMarker = L.marker([52.23, 21.011111]).addTo(map).bindPopup('<a href="https://www.wtp.waw.pl/rozklady-jazdy/" target="_blank">Warsaw</a>');
+let parisMarker = L.marker([48.8567, 2.3522]).addTo(map).bindPopup('<a href="https://www.ratp.fr/en/horaires-bus" target="_blank">Paris</a>');
+let munichMarker = L.marker([48.1375, 11.575]).addTo(map).bindPopup('<a href="https://efa.mvv-muenchen.de/index.html#timetables@enquiry" target="_blank">Munich</a>');
 
 // Quicker map refresh
 const mapElement = document.getElementById('map');
